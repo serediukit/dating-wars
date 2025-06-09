cc2fb577570e02a570731d58536f2d054e744149
serediukit <serediukit@gmail.com>
2025-06-09 23:37:58 +0300

    TASK-1: add simple routing

--- server.go
+++ server.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"username"`
+	Email string `json:"email"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Dating Wars Server")
+var users []User
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	newUser := User{}
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	users = append(users, newUser)
+	err = json.NewEncoder(w).Encode(newUser)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
